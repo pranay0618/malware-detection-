e427e79c574024a25d210134546373ca6a8fdc0e
pranay0618 <112579318+pranay0618@users.noreply.github.com>
2023-06-25 21:42:23 +0530

    Add files via upload

---
+++ Malware-detection-using-spatiotemporal-pattern-main/Malware-detection-using-spatiotemporal-pattern-main/dark_tracer_early_detection/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for dark_tracer_early_detection
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dark_tracer_early_detection.settings')
+
+application = get_asgi_application()
